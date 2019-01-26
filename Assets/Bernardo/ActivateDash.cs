570880f9e1f3673e6d2c0dbc03685ce92e9e08c3
Villaroux <37813904+Villaroux@users.noreply.github.com>
2019-01-26 19:27:57 +0000

    RemakePlayrController

---
+++ Assets/Bernardo/ActivateDash.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ActivateDash : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+
+        if(player != null)
+        {
+            player.IsDashActivated = true;
+        }
+    }
+}
